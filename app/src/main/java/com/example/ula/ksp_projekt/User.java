0633b6e5995ab1c54860bfa58036eb4abac433b9
liilith93 <urszmol393@student.polsl.pl>
2017-07-19 22:35:29 +0200

    ver. 1.0

---
+++ app/src/main/java/com/example/ula/ksp_projekt/User.java
@@ -0,0 +1,13 @@
+package com.example.ula.ksp_projekt;
+
+/**
+ * Created by Ula on 15.07.2017.
+ */
+
+public class User {
+    public String name;
+
+    public User(String name){
+        this.name = name;
+    }
+}
