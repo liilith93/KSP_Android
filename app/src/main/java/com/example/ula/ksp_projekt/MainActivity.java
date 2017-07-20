28ee43a442a954bbe71ed2f48f974213ad7fb961
liilith93 <urszmol393@student.polsl.pl>
2017-07-20 23:47:51 +0200

    ver. 1.0.1

--- app/src/main/java/com/example/ula/ksp_projekt/MainActivity.java
+++ app/src/main/java/com/example/ula/ksp_projekt/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ula.ksp_projekt;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,9 @@ import com.google.gson.GsonBuilder;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     Button disconnect;
     EditText txtName;
     EditText txtIP;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,33 +72,12 @@ public class MainActivity extends AppCompatActivity {
                         token = callback.token;
 
 
-                        Call<Measurment> measure = service.measurements(token);
-                        measure.enqueue(new Callback<Measurment>() {
-                            @Override
-                            public void onResponse(Call<Measurment> call, Response<Measurment> response) {
-                                if (response.code() == 200) {
-                                    Measurment pomiar = response.body();
-                                    int temperature = pomiar.temperature;
-                                    int humidity = pomiar.humidity;
-                                    String update = pomiar.lastUpdate;
-                                    txtResp.setText("Temperatura: " + String.valueOf(temperature) + "\n" +
-                                            "Wilgotność: " + String.valueOf(humidity) + "\n" +
-                                            "Ostatnia aktualizacja: " + update);
-                                    disconnect.setEnabled(true);
-                                    connect.setEnabled(false);
-                                } else if (response.code() == 401) {
-                                    txtResp.setText("Brak autoryzacji, podaj imię i połącz się ponownie z serwerem!");
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(Call<Measurment> call, Throwable throwable) {
-                                txtResp.setText("Problem z pobraniem danych! \n" + throwable.getMessage());
-                            }
-                        });
+                        callAsyncTask();
                     }
                     else if(response.code() == 400){
                         txtResp.setText("Podaj parametry połączenia!");
+                        timer.cancel();
+                        timer.purge();
                     }
                 }
 
@@ -101,11 +85,15 @@ public class MainActivity extends AppCompatActivity {
                 public void onFailure(Call<ServerResponse> call, Throwable throwable) {
                     throwable.printStackTrace();
                     txtResp.setText("Problem z połączeniem! \n" + throwable.getMessage());
+                    timer.cancel();
+                    timer.purge();
                 }
             });
         }
         catch(Exception e){
             txtResp.setText("Niepoprawne dane połączenia!");
+            timer.cancel();
+            timer.purge();
         }
     }
 
@@ -120,17 +108,84 @@ public class MainActivity extends AppCompatActivity {
                     connect.setEnabled(true);
                     txtName.setText("");
                     txtIP.setText("http://");
+                    timer.cancel();
+                    timer.purge();
                 }
                 else
                 {
                     txtResp.setText("Błąd podczas rozłączania!");
+                    disconnect.setEnabled(false);
+                    connect.setEnabled(true);
+                    txtName.setText("");
+                    txtIP.setText("http://");
+                    timer.cancel();
+                    timer.purge();
                 }
             }
 
             @Override
             public void onFailure(Call<ServerResponse> call, Throwable throwable) {
                 txtResp.setText("Błąd podczas rozłączania!");
+                disconnect.setEnabled(false);
+                connect.setEnabled(true);
+                txtName.setText("");
+                txtIP.setText("http://");
+                timer.cancel();
+                timer.purge();
             }
         });
     }
+
+    public void callAsyncTask(){
+        final Handler handler = new Handler();
+        timer = new Timer();
+        TimerTask doAsyncTask = new TimerTask(){
+
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            Call<Measurment> measure = service.measurements(token);
+                            measure.enqueue(new Callback<Measurment>() {
+                                @Override
+                                public void onResponse(Call<Measurment> call, Response<Measurment> response) {
+                                    if (response.code() == 200) {
+                                        Measurment pomiar = response.body();
+                                        int temperature = pomiar.temperature;
+                                        int humidity = pomiar.humidity;
+                                        String update = pomiar.lastUpdate;
+                                        txtResp.setText("Temperatura: " + String.valueOf(temperature) + "\n" +
+                                                "Wilgotność: " + String.valueOf(humidity) + "\n" +
+                                                "Ostatnia aktualizacja: " + update);
+                                        disconnect.setEnabled(true);
+                                        connect.setEnabled(false);
+                                    } else if (response.code() == 401) {
+                                        txtResp.setText("Brak autoryzacji, podaj imię i połącz się ponownie z serwerem!");
+                                        timer.cancel();
+                                        timer.purge();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<Measurment> call, Throwable throwable) {
+                                    txtResp.setText("Problem z pobraniem danych! \n" + throwable.getMessage());
+                                    connect.setEnabled(true);
+                                    disconnect.setEnabled(false);
+                                    timer.cancel();
+                                    timer.purge();
+                                }
+                            });
+                        } catch(Exception e){
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(doAsyncTask, 0, 10000);
+
+    }
 }
+
+
